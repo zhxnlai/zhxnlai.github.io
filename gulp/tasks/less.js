b1e3c98d7c8b9991ab6cd7cdf25efc053c55db38
Zhixuan Lai <zhxnlai@gmail.com>
2015-04-11 13:55:54 -0700

    adding auto prefixer

--- gulp/tasks/less.js
+++ gulp/tasks/less.js
@@ -1,17 +1,18 @@
 var gulp = require('gulp'),
-  minifyCSS = require('gulp-minify-css'),
   less = require('gulp-less'),
   sourcemaps = require('gulp-sourcemaps'),
   handleErrors = require('../util/handleErrors'),
   config=require('../config').less;
-  var uglifycss = require('gulp-uglifycss');
+var uglifycss = require('gulp-uglifycss');
+var postcss      = require('gulp-postcss');
+var autoprefixer = require('autoprefixer-core');
 
 gulp.task('less', function() {
   return gulp.src(config.src)
     .pipe(sourcemaps.init())
     .pipe(less())
     .on('error', handleErrors)
-    .pipe(minifyCSS({keepBreaks:true}))
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
     .pipe(uglifycss({
       maxLineLen: 80
     }))
